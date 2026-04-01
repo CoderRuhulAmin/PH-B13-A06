312464c01f866a540ff24ff96a468ed23ea41943
CoderRuhulAmin <coderruhulamin@gmail.com>
2026-04-01 21:06:49 +0600

    handling the delete button in cartList is done.

--- src/modules/main/components/CartList.jsx
+++ src/modules/main/components/CartList.jsx
@@ -3,11 +3,27 @@ import React from 'react';
 import CartListItem from './CartListItem';
 
 const CartList = ({ cartList, setCartList }) => {
+    
+    const handleDeleteSelectedProduct = (item) => {
+        // console.log(cartList );
+        // const filteredCartList = cartList.filter(
+        //     (selectedCartItem) => selectedCartItem.id !== item.id,
+        // );
+        // console.log(filteredCartList);
+        // setCartList(filteredCartList);
+
+        setCartList(prev =>
+            prev.filter(cartItem => cartItem.id !== item.id)
+        );
+        
+    }
+
+
     if (cartList.length === 0) {
         return (
-            <div>
-                <h3>Your Cart</h3>
-                <p>No Product add to cart.</p>
+            <div className='flex flex-col justify-center items-center py-10 bg-gray-100 space-y-4'>
+                <h3 className='text-3xl font-bold'>No product is added to Cart!</h3>
+                <p>Go to product list and select your favourite product to cart.</p>
             </div>
         )
     }else{
@@ -16,7 +32,11 @@ const CartList = ({ cartList, setCartList }) => {
                 <h3>Your Cart</h3>
                 <ul className="list bg-white rounded-box space-y-4">
                     {
-                        cartList.map(item => <CartListItem key={item.id} item={item} />)
+                        cartList.map(item => <CartListItem 
+                            key={item.id} 
+                            item={item} 
+                            handleDeleteSelectedProduct={handleDeleteSelectedProduct}
+                        />)
                     }
                 </ul>
             </div>
--- src/modules/main/components/CartListItem.jsx
+++ src/modules/main/components/CartListItem.jsx
@@ -1,6 +1,7 @@
 import { Trash2 } from 'lucide-react';
 
-const CartListItem = ({item}) => {
+const CartListItem = ({item, handleDeleteSelectedProduct}) => {
+
     return (
         <li className="list-row bg-gray-50 rounded-lg">
             <div className="text-4xl font-thin opacity-30 tabular-nums">{item.id}</div>
@@ -9,7 +10,7 @@ const CartListItem = ({item}) => {
                 <div>{item.name}</div>
                 <div className="text-xs uppercase font-semibold opacity-60">${item.price}/{item.period}</div>
             </div>
-            <button className="btn btn-square btn-ghost">
+            <button onClick={()=>handleDeleteSelectedProduct(item)} className="btn btn-square btn-ghost">
                 <Trash2 />
             </button>
         </li>
